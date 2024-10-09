43d56a9a861dba2d4284d62c1c245865ff5cb919
Disaster <Disaster20031017@gmail.com>
2024-10-09 13:21:31 +0900

    Update Background

---
+++ Assets/2_Play/Scripts/MoveBackground.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // ”wŒi‘JˆÚ
+        transform.position += new Vector3(-5 * Time.deltaTime, 0);
+        if(transform.position.x <= -38)
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
